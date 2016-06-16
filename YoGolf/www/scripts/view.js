d62a0e8a57fe7e82a736d35896daff8563e8b7a6
Martin Řezník <marez@microsoft.com>
2016-06-16 23:17:44 +0200

    Adding layout page

--- YoGolf/www/scripts/view.js
+++ YoGolf/www/scripts/view.js
@@ -3,9 +3,12 @@
 View.Courses = function (db) {
     Course.WithAll(db, function (courses) {
         $(':mobile-pagecontainer').pagecontainer('change', $('#courses'));
+        $("#courses .content").html("");
         $.each(courses, function (k, course) {
             var layouts = course.layouts.map(function (layout) {
-                return $('<li>').text(layout.name + ': ' + layout.paths.length + ' holes | par ' + layout.par() + ' | ' + layout.length().toFixed(0) + ' meters');
+                return $('<li>')
+                    .text(layout.name + ': ' + layout.paths.length + ' holes | par ' + layout.par() + ' | ' + layout.length().toFixed(0) + ' meters')
+                    .data("layout", layout);
             });
             $('#courses .content')
                 .append($('<div class="course">')
@@ -20,9 +23,32 @@ View.Courses = function (db) {
             $(item).append($('<span class="distance">'));
         });
         $('#courses .content h2').click(function (event) {
-            console.log(this, $(this).closest('div.course').find("ul.layouts"));
             $(this).closest('div.course').find("ul.layouts").toggleClass("hidden");
         });
+        $('#courses .content .layouts li').click(function (event) {
+            View.Layout($(this).data("layout"));
+        });
+    });
+}
+
+View.Layout = function (layout) {
+    $(':mobile-pagecontainer').pagecontainer('change', $('#layout'));
+    console.log(layout);
+    $("#layout .content").html("");
+    $("#layout .content").append($("<h2>").text(layout.course.name + ": " + layout.name));
+    $("#layout .content").append($('<ul class="paths">'));
+    $.each(layout.paths, function (k, path) {
+        $("#layout .content ul.paths").append(
+            $("<li>")
+                .text('[' + path.number + '] ' + path.tee.name + ' ➟ ' + path.basket.name + ':  par ' + path.par + ' | ' + path.distance().toFixed(0) + ' meters | ')
+                .append($('<span class="showCompass showDistance" coords="' + path.tee.coord().toString() + '"></span>'))
+        );
+    });
+    $(".showCompass").each(function (k, item) {
+        $(item).append($("#templates .compass").clone());
+    });
+    $(".showDistance").each(function (k, item) {
+        $(item).append($('<span class="distance">'));
     });
 }
 
